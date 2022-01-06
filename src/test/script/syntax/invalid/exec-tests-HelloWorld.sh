ad5a9cdda195171f9b0dd950e6d4d5f9293a7232
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-06 22:53:39 +0100

    Epuration des scripts.
    Les scripts sont maintenant un peu plus factorisés.

---
+++ src/test/script/syntax/invalid/exec-tests-HelloWorld.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+# Author : gl49, Paul
+# Initial version : 06/01/2022
+
+# This program is able to execute all the invalid syntax tests
+# for the HelloWorld step.
+# Associated results will be found in ../../tmp
+# under the suffixes .listmp
+cd "$(dirname "$0")"/../../../../../ || exit 1
+
+# We change the paths to execute the tests from the project root.
+PATH=src/test/script/launchers:"$PATH"
+TESTPATH=src/test/deca/syntax/invalid/HelloWorld
+TMP=src/test/tmp
+
+# Coloring for the script.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Function to verify if the test is invalid. If so, it will be stored in a
+# temporary result repository.
+
+test_synt_invalid () {
+    # $1 = first argument : name of files without extension
+
+    # echo $(test_synt $TESTPATH/$1.deca)
+    if test_synt $TESTPATH/$1.deca 2>&1 | grep -q -e "$1.deca:[0-9][0-9]*:"
+        then # normal fail
+            test_synt $TESTPATH/$1.deca 1> $TMP/$1.listmp 2>> $TMP/$1.listmp
+            echo "${GREEN}[KO] : $1 ${NC}"
+        else # unexpected success
+            echo "${RED}[OK] : $1 ${NC}"
+	    exit 1
+    fi
+}
+
+
+# looping on all the targeted paths
+for cas_de_test in "$TESTPATH"/*.deca
+do	
+    file=$(basename "$cas_de_test" ".deca")
+    test_synt_invalid "$file"
+done
---
+++ src/test/script/syntax/valid/regression-tests-HelloWorld.sh
@@ -0,0 +1,61 @@
+#! /bin/sh
+
+# Author : gl49, Paul
+# Initial version : 06/01/2022
+
+# This program is able to execute all the valid syntax tests
+# for the HelloWorld step and compare them with the validated
+# one found in ../../../results/deca/syntax/valid/HelloWorld
+# If we find a difference, then we must identify the regression
+# source.
+cd "$(dirname "$0")"/../../../../../ || exit 1
+
+# We change the paths to execute the tests from the project root.
+PATH=src/test/script/launchers:"$PATH"
+TESTPATH=src/test/deca/syntax/valid/HelloWorld
+LISPATH=src/test/results/deca/syntax/valid/HelloWorld
+TMP=src/test/tmp
+
+# Coloring for the script.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Function to verify that there was no regression between old results
+# and new ones.
+regression_test () {
+    # $1 = first argument : name of files without extension
+    
+    # Checks if an old file was there.
+    # echo "$LISPATH/$1.lis"
+    if [ ! -f $LISPATH/$1.lis ]
+        then
+            echo "${RED}[NO OLD] : $1 ${NC}"
+	    exit 2
+    fi
+
+    # Checks if a new result file was created.
+    # echo "$TMP/$1.listmp"
+    if [ ! -f $TMP/$1.listmp ]
+        then
+           echo "${RED}[NO NEW] : $1 ${NC}"
+	   exit 3
+    fi
+		
+    DIFF=$(diff $LISPATH/$1.lis $TMP/$1.listmp)
+    if [ "$DIFF" != "" ]
+        then
+            echo "${RED}[REGRESSION] : $1 ${NC}"
+            exit 1
+        else
+            echo "${GREEN}[NO-REGRESSION] : $1 ${NC}"
+    fi
+}
+
+# Looping on all the targeted paths.
+for cas_de_test in "$TESTPATH"/*.deca
+do
+# we get the filename without its extension for further use
+    file=$(basename "$cas_de_test" ".deca")
+    regression_test "$file"
+done
