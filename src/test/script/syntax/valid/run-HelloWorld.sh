d6dac578ef5da038e71c1b39ef7079c86cf433cb
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-06 19:04:40 +0100

    Légère retouche d'explication

--- src/test/script/syntax/valid/run-HelloWorld.sh
+++ src/test/script/syntax/valid/run-HelloWorld.sh
@@ -17,20 +17,20 @@ TMP=./src/test/tmp
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-# we will use two LISPATH repository : one with already saved
+# we will use two repositories : one with already saved
 # files and another with the newly created results
 
 
 # function to verify if the test is valid, if so it is stored in a
 # temporary result repository
 test_synt_valid () {
-    # $1 = first argument
+    # $1 = first argument : name of files without extension
+    echo $(test_synt "$1") 
     if test_synt "$1" 2>&1 | head -n 1 | grep -q "$1.deca"
         then # abnormal success
             echo "${RED}[KO] : $1 ${NC}"
             exit 1
         else # normal success : we store the new result in a tmp file
-            touch "$TMP"/"$1".listmp
             test_synt "$1".deca 1> "$TMP"/"$1".listmp 2>> "$TMP"/"$1".listmp
             echo "${GREEN}[OK] : $1 ${NC}"
     fi
@@ -39,7 +39,7 @@ test_synt_valid () {
 # function to verify that there was no regression between old results
 # and new ones
 no_regression_test () {
-    if [ diff $LISPATH/$1.lis $TMP/$1.tmp ]
+    if [ diff $LISPATH/$1.lis $TMP/$1.listmp ]
         then
             echo "${RED}[REGRESSION] : $1 ${NC}"
             exit 1
@@ -48,9 +48,12 @@ no_regression_test () {
     fi
 }
 
+./src/test/script/basic-synt.sh
+
 # looping on all the targeted paths
 for cas_de_test in "$TESTPATH"/*.deca
-do
+do	
+    cat $cas_de_test
     file=$(basename "$cas_de_test" ".deca")
     test_synt_valid "$file"
 done
