e875d24bcdf82f104dcdb72b5d3eb91bde5c56d7
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-12 16:08:57 +0100

    Début de la modification de la classe GPRegister
    Ajout de commentaires pour les méthodes

--- src/main/java/fr/ensimag/ima/pseudocode/GPRegister.java
+++ src/main/java/fr/ensimag/ima/pseudocode/GPRegister.java
@@ -7,31 +7,42 @@ package fr.ensimag.ima.pseudocode;
  * @date 01/01/2022
  */
 public class GPRegister extends Register {
+    
+    private int number;
+
+    private boolean availability = true;
+
     /**
      * @return the number of the register, e.g. 12 for R12.
      */
     public int getNumber() {
         return number;
     }
+    
+    /**
+     * @return true if the register is available for use, else false
+     */
+    public boolean available(){
+        return availability;
+    }
 
-    private int number;
-
-    private boolean availability = true;
-
+    
     GPRegister(String name, int number) {
         super(name);
         this.number = number;
     }
 
-    public boolean available(){
-        return availability;
-    }
-
+    /**
+     * Makes the register unavailable for saves
+     */
     public void use(){
         assert(availability);
         availability=false;
     }
 
+    /**
+     * Free an unavailable register to make it usable for saves
+     */
     public void liberate(){
         assert(!availability);
         availability=true;
