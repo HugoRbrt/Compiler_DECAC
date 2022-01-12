ea193cabcedd6d47dd0bc283e36c9ac2ead2fdd7
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-12 22:24:50 +0100

    Nouveau : code Analyzer qui enregistre les push et pop
    effectués pour récupérer facilement le nombre de variables
    déclarées.

---
+++ src/main/java/fr/ensimag/deca/tools/CodeAnalyzer.java
@@ -0,0 +1,60 @@
+package fr.ensimag.deca.tools;
+
+/**
+ * Global code analyzer used during code generation to
+ * compute the dynamic variables used for error portions
+ * and label generation
+ */
+
+
+public class codeAnalyzer {
+
+    /**
+    * Number that keeps track of declared variables
+    */
+    private int nbDeclaredVariables = 0;
+    
+    /**
+    * Number that keeps track of the needed size of memory in
+    * stack to compute the instructions
+    */
+    private int stackSizeInstructions = 0;
+    
+    /**
+     * Special counter to keep track of a the number of PUSH - POP
+     */
+    private int diffPushPop = 0;
+    
+    public int getNbDeclaredVariables() {
+        return nbDeclaredVariables;
+    }
+    
+    public int getStackSizeInstructions() {
+        return stackSizeInstructions;
+    }
+
+    /**
+    * @args nbPop : number of consecutive POP instructions to be declared
+    */
+    public void incrPopCount(int nbPop) {
+        diffPushPop += nbPop;
+        if (diffPushPop > stackSizeInstructions) {
+            stackSizeInstructions = diffPushPop;
+        }
+    }
+
+    /**
+    * @args nbPush : number of consecutive PUSH instructions to be declared
+    */
+    public void incrPushCount(int nbPush) {
+        diffPushPop -= nbPush;
+    }
+
+    /**
+     * @args nbVariables : number of declared variables in a row
+     */
+    public void incrDeclaredVariables(int nbVariables) {
+        nbDeclaredVariables += nbVariables;
+    }
+    
+}
