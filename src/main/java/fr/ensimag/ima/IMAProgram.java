c598f2024713a67fecddf0b9963352aa0960b31a
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-13 12:25:41 +0100

    Début du travail sur les messages d'erreurs

--- src/main/java/fr/ensimag/ima/IMAProgram.java
+++ src/main/java/fr/ensimag/ima/IMAProgram.java
@@ -16,4 +16,17 @@ public class IMAProgram  extends GenericProgram{
     public void addComment(String s) {
         lines.add(new Line(s));
     }
+    
+    /**
+     * @param d1 : needed stack size
+     * @param d2 : number of declared variables
+     * Adds the first instruction of TSTO check and ADDSP in the list of lines
+     */
+    public void addTstoCheck(int d1, int d2) {
+        lines.addFirst(new Line()); // changer ici ADDSP d2
+        lines.addFirst(new Line()); // changer ici BEQ 
+        lines.addFirst(new Line()); // changer ici TSTO d1
+    }
+    
+    
 }
