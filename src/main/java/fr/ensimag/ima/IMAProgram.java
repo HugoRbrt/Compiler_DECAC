7e51044937600be95fea8878157846f3c3bfdb99
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-13 13:58:35 +0100

    Nouvelle fonction (à tester) addTstoCheck dans l'IMAProgram

--- src/main/java/fr/ensimag/ima/IMAProgram.java
+++ src/main/java/fr/ensimag/ima/IMAProgram.java
@@ -1,8 +1,12 @@
 package fr.ensimag.ima.pseudocode;
+import fr.ensimag.ima.pseudocode.instructions.ADDSP;
+import fr.ensimag.ima.pseudocode.instructions.TSTO;
+import fr.ensimag.ima.pseudocode.instructions.BOV;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.LinkedList;
+
 
 /**
  * Abstract representation of an IMA program, i.e. set of Lines.
@@ -22,14 +26,11 @@ public class IMAProgram  extends GenericProgram{
      * @param d2 : number of declared variables
      * Adds the first instruction of TSTO check and ADDSP in the list of lines
      */
-    
-    /*
-    *public void addTstoCheck(int d1, int d2) {
-    *    lines.addFirst(new Line()); // changer ici ADDSP d2
-     *   lines.addFirst(new Line()); // changer ici BEQ 
-     *   lines.addFirst(new Line()); // changer ici TSTO d1
-    *}
-    */
+    public void addTstoCheck(int d1, int d2) {
+        lines.addFirst(new Line(new ADDSP(d2)));
+        lines.addFirst(new Line(new BOV(new Label("stack_overflow"))));
+        lines.addFirst(new Line(new TSTO(d1)));
+    }
     
     
 }
