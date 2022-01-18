5093f9de186602f60570b61eb5e40f2f877e5e99
robert hugo <roberthugo@icloud.com>
2022-01-19 00:00:45 +0100

    modif instanceof

--- src/main/java/fr/ensimag/deca/tree/InstanceOf.java
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -48,7 +48,8 @@ public class InstanceOf extends AbstractExpr {
         Label success = new Label();
         Label failure = new Label();
         Label end = new Label();
-        compiler.addInstruction(new LEA(  compiler.getstackTable().get(((Identifier) expr).getName()) , Register.R0));
+        System.out.println(((Identifier) expr).getType().getName());
+        compiler.addInstruction(new LEA(  compiler.getstackTable().get( compiler.getSymbTable().get(expr.getType().getName().getName())) , Register.R0));
         compiler.addInstruction(new LEA(  compiler.getstackTable().get( comparedTo.getName()) , Register.R1));
         compiler.addLabel(start);
         compiler.addInstruction(new CMP(Register.R0, Register.R1));
