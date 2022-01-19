1a6f3c089d427464aab08e0304ce963c5e8be74e
robert hugo <roberthugo@icloud.com>
2022-01-19 14:05:47 +0100

    instanciation et affichage de cast fonctionnel

--- src/main/java/fr/ensimag/deca/tree/Selection.java
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -4,12 +4,10 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.ImmediateFloat;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.RegisterOffset;
-import fr.ensimag.ima.pseudocode.instructions.LOAD;
-import fr.ensimag.ima.pseudocode.instructions.WFLOAT;
-import fr.ensimag.ima.pseudocode.instructions.WFLOATX;
-import fr.ensimag.ima.pseudocode.instructions.WINT;
+import fr.ensimag.ima.pseudocode.instructions.*;
 
 import java.io.PrintStream;
 
@@ -84,10 +82,12 @@ public class Selection extends AbstractLValue {
 
     protected void codeGenPrint(DecacCompiler compiler, boolean printHex) {
         //on recupère l'adresse de l'objet dans R0
-
-
-        RegisterOffset r = compiler.getstackTable().get(compiler.getSymbTable().get(((Identifier)selectingClass).getName().getName()));
-        compiler.addInstruction(new LOAD(r, Register.R0));
+        if(selectingClass instanceof Cast){
+            selectingClass.codeGenInst(compiler);
+        }else{
+            RegisterOffset r = compiler.getstackTable().get(compiler.getSymbTable().get(((Identifier)selectingClass).getName().getName()));
+            compiler.addInstruction(new LOAD(r, Register.R0));
+        }
         compiler.addInstruction(new LOAD(new RegisterOffset( selectedField.getFieldDefinition().getIndex() , Register.R0), Register.R1));
         if(selectedField.getType().isInt()){
             compiler.addInstruction(new WINT());
