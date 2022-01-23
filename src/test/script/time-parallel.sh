db8032e31751b5db47c1ce60a3d9008767f1c3a0
Paul Marthelot <paul.marthelot@grenoble-inp.org>
2022-01-23 23:43:54 +0100

    Modification du time-parallel.sh

--- src/test/script/time-parallel.sh
+++ src/test/script/time-parallel.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 echo "Compilation without -P:"
-time decac -p ../deca/context/invalid/SansObjet/*.deca > /dev/null
+time decac -p ../deca/context/valid/SansObjet/*.deca > /dev/null
 echo "Compilation with -P:"
-time decac -p -P ../deca/context/invalid/SansObjet/*.deca > /dev/null
+time decac -p -P ../deca/context/valid/SansObjet/*.deca > /dev/null
